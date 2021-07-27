cf5708453769896efd5670340e959f0e48a250a8
Damian Tarnawsky <damian@ionic.io>
2021-07-27 09:00:17 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cs-iab',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

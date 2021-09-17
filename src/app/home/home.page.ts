352cec307b91d5ba1640bdc0efe37019222dcf01
Damian Tarnawsky <damian@ionic.io>
2021-09-17 09:02:03 -0700

    Code cleanup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,26 +10,42 @@ import { Platform } from '@ionic/angular';
 export class HomePage {
   private browser: InAppBrowserObject;
   constructor(private iab: InAppBrowser, private platform: Platform) {
-    platform.ready().then(() => {
+    this.platform.ready().then(() => {
       this.open('https://cs-links.netlify.app/');
     });
   }
 
-  public open(url: string) {
-    this.browser = this.iab.create(url, '_blank', 'location=no,beforeload=yes');
+  // This is an example of code that will modify all hyperlinks on the page
+  // Add this to executeScript if you want to use it
+  private fixLinks(): string {
+    return 'for (let link of document.getElementsByTagName("a")) {' +
+      '   if (link.href.endsWith("/")) {' +
+      '      link.href = link.href.slice(0, -1);' + // Removes trailing slashes on links
+      '      link.target = "_system";' + // Ensures the target for any links is _blank
+      '      console.log(\'Fix Link\', link.href);' +
+      ' }' +
+      '};';
+  }
 
-    this.browser.on('loadstop').subscribe((event: InAppBrowserEvent) => {
+  public open(url: string) {
+    console.log(`open ${url}`);
+    this.browser = this.iab.create(url, '_blank',
+      {
+        location: 'no',
+        beforeload: 'yes',
+        suppressesIncrementalRendering: 'yes'
+      });
+
+    this.browser.on('loadstop').subscribe(async (event: InAppBrowserEvent) => {
       console.log(`loadstop ${JSON.stringify(event)}`);
 
       // This injects code on the page to wait for changes in the browser location and sends a message with the url to the application
-      this.browser.executeScript({
-        code: 'setInterval(() => { if (window.lastHref != location.href) { let data = JSON.stringify({href: location.href}); window.lastHref = location.href; try { webkit.messageHandlers.cordova_iab.postMessage(data); } catch (err) { console.error(err); } } },100);'
+      await this.browser.executeScript({
+        code: 'setInterval(() => { if (window.lastHref != location.href) { var data = JSON.stringify({href: location.href}); window.lastHref = location.href; try { webkit.messageHandlers.cordova_iab.postMessage(data); } catch (err) { console.error(err); } } },100);'
       });
     });
 
     this.browser.on('loadstart').subscribe((event: InAppBrowserEvent) => {
-      // Fires on iOS
-      // Fires on Android
       console.log(`loadstart ${JSON.stringify(event)}`);
     });
 
@@ -38,29 +54,28 @@ export class HomePage {
     });
 
     this.browser.on('beforeload').subscribe((event: InAppBrowserEvent) => {
-      // Fires on iOS
-      // Does not fire on Android on startup of first browser load but does fire afterwards
       console.log(`beforeload ${JSON.stringify(event)}`);
 
-      if (event.url.toLowerCase().endsWith(".pdf")) {
-        // We want to launch this in the system browser instead of from InAppBrowser
+      // We want to launch this in the system browser instead of from InAppBrowser
+      if (event.url.toLowerCase().startsWith("https://ionic")) {
         console.log(`Opening ${event.url} in system browser`);
         window.open(event.url, '_system', 'location=yes');
         return;
       }
 
-      if (!event.url.startsWith("https://")) {
-        // Avoid urls other than https
-        console.log(`Unsupported url ${event.url}`);
+      // We want to launch this in the system browser instead of from InAppBrowser
+      if (event.url.toLowerCase().endsWith(".pdf")) {
+        console.log(`Opening ${event.url} in system browser`);
+        window.open(event.url, '_system', 'location=yes');
         return;
       }
 
-      // Callback to ensure it loads
+      // Callback to ensure the url loads
+      console.log('_loadAfterBeforeLoad ' + event.url);
       this.browser._loadAfterBeforeload(event.url);
     });
 
     this.browser.on('message').subscribe((event: InAppBrowserEvent) => {
-
       // This will output the object in event.data. You could reference event.data.href
       console.log(`message`, event.data);
     });
